6e4153907c578d32ba131106f739f2234bac6a4e
AndyLiu11037 <z5161216@unsw.edu.au>
2019-12-31 14:38:12 +0800

    Updates

---
+++ precache-manifest.bcc5244b6b8f859f6172ab0349ddb1e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8df728f49ba5548ef4f46c6863a51c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f6858ff06c403e4e3aa",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9b1cd27a09b1c2d951a8",
+    "url": "/static/css/main.c899126d.chunk.css"
+  },
+  {
+    "revision": "6f6858ff06c403e4e3aa",
+    "url": "/static/js/2.17d9e13e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.17d9e13e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b1cd27a09b1c2d951a8",
+    "url": "/static/js/main.e4fc54b2.chunk.js"
+  },
+  {
+    "revision": "fa320196e08177e6e7dc",
+    "url": "/static/js/runtime-main.aaa91877.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.f9ab4fb0f01e8d189bfe6cc40ac9ddf6.js"
+  "/precache-manifest.bcc5244b6b8f859f6172ab0349ddb1e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
