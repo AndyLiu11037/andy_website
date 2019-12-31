554b42a4a035a2693d724c5445d7ca301f302b38
AndyLiu11037 <z5161216@unsw.edu.au>
2019-12-31 17:59:35 +0800

    creating router links

--- my-website/src/Components/NavBar.js
+++ my-website/src/Components/NavBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link, BrowserRouter as Router} from 'react-router-dom'
 import './../App.css'
 import './../fonts.css'
 class NavBar extends Component{
@@ -7,9 +8,12 @@ class NavBar extends Component{
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="website-name">Andy Liu's Resume</div>
                 <div className="navbar-nav ml-5">
-                    <a className="nav-item nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
-                    <a className="nav-item nav-link" href="/about">About</a>
-                    <a className="nav-item nav-link" href="/projects">Past Projects</a>
+                    <Router>
+                        <a className="nav-item nav-link"> <Link to="/home"></Link> Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link" href="/about">About</a>
+                        <a className="nav-item nav-link" href="/projects">Past Projects</a>
+                    </Router>
+
                 </div>
             </nav>
         )
