fb427a53291189ecf8a73afcefa9e8dfa7e81d70
AndyLiu11037 <z5161216@unsw.edu.au>
2019-12-31 15:17:22 +0800

    successfully deploy page to github pages
    updating the css

--- my-website/src/Components/NavBar.js
+++ my-website/src/Components/NavBar.js
@@ -4,17 +4,13 @@ import './../fonts.css'
 class NavBar extends Component{
     render(){
         return(
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="website-name">Andy Liu's Resume</div>
-
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Features</a>
-                    <a class="nav-item nav-link" href="#">Pricing</a>
-                    <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="website-name">Andy Liu's Resume</div>
+                <div className="navbar-nav ml-5">
+                    <a className="nav-item nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-item nav-link" href="/about">About</a>
+                    <a className="nav-item nav-link" href="/projects">Past Projects</a>
                 </div>
-            </div>
             </nav>
         )
 
